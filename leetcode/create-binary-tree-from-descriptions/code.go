c2fd602fb44f2113b3b2d7c8f80a765b05817a6d
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2026-06-07 21:04:15 +0700

    feat: create-binary-tree-from-descriptions

---
+++ leetcode/create-binary-tree-from-descriptions/code.go
@@ -0,0 +1,45 @@
+package createbinarytreefromdescriptions
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func createBinaryTree(descriptions [][]int) *TreeNode {
+	registry := make(map[int]*TreeNode)
+	childSet := make(map[int]struct{})
+	for i := range descriptions {
+		val := descriptions[i][0]
+		child := descriptions[i][1]
+		isLeft := descriptions[i][2] == 1
+		node, ok := registry[val]
+		if !ok {
+			node = &TreeNode{
+				Val: val,
+			}
+			registry[val] = node
+		}
+		registry[val] = node
+		childNode, ok := registry[child]
+		if !ok {
+			childNode = &TreeNode{
+				Val: child,
+			}
+			registry[child] = childNode
+		}
+		if isLeft {
+			node.Left = childNode
+		} else {
+			node.Right = childNode
+		}
+		childSet[descriptions[i][1]] = struct{}{}
+	}
+	for i := range descriptions {
+		val := descriptions[i][0]
+		if _, ok := childSet[val]; !ok {
+			return registry[val]
+		}
+	}
+	return nil
+}
